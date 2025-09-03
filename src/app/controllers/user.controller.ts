2518078426063502761f70d2d04992fb790a8317
Athiqur01 <athik.apece@gmail.com>
2025-09-03 11:36:56 +0600

    user

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -12,3 +12,30 @@ userRoute.post("/create-user", async(req: Request, res: Response)=>{
         user: createUser
     })
 })
+// get operation
+userRoute.get("/", async(req: Request, res: Response)=>{
+    const users=await User.find()
+    res.status(201).json({
+        success: true,
+        message: "User created successfully",
+        users: users})
+})
+//get operation for single user
+userRoute.get("/:userId", async(req: Request, res: Response)=>{
+    const userId=req.params.userId
+    const users=await User.findOne({_id: userId})
+    res.status(201).json({
+        success: true,
+        message: "User created successfully",
+        users: users})
+})
+//update operation
+userRoute.patch("/update-user/:userId", async(req: Request, res: Response)=>{
+    const userId=req.params.userId
+    const userData=req.body
+    const updateUser= await User.findByIdAndUpdate(userId, userData, {new: true})
+    res.status(201).json({
+        success: true,
+        message: "User created successfully",
+        users: updateUser})
+})
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -2,13 +2,27 @@ import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
 
 const userSchema= new Schema<IUser>({
-    firstName:{type: String, required: true, trim: true},
+    firstName:{
+        type: String, 
+        required: [true, "First Name is required"], 
+        trim: true, 
+        minLength:[3, 'First Name length Must be at least 3, got {VALUE}'],
+        maxLength:[10, 'First Name length Must be at least 3, got {VALUE}'],
+    },
     lastName:{type: String, required: true, trim: true},
-    email: {type: String, required: true},
+    email: {
+        type: String, 
+        required: true, 
+        lowercase:true, 
+        unique:[true, "Provide unique email address"]},
+    age: {type:Number, min:[18,"Age must be at least 18, got{VALUE}"], max: [50,"Age would be maximum 50, got{VALUE}"]},
     password: {type: String, required: true},
     role:{
         type: String,
-        enum: ["admin", "user"],
+        enum: {
+            values:['user', 'admin'],
+            message: "Provide valid user. Got {VALUE}"
+        },
         default: "user"
     }
 })
